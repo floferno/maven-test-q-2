9cf997ffa50f5c4d6c0a37e6098cb5fbb784c41d
floferno <olfiarachmi@gmail.com>
2022-06-27 09:10:26 +0800

    Change color of each bar (there are still some bugs)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### How to run
 
 > live-server --localhost:5500
+
+### Results
+* Result under 3 seconds after user inputs number
+  ![Result under 3 seconds after user inputs number](before3sec.png "Result under 3 seconds after user inputs number")
--- script.js
+++ script.js
@@ -1,106 +1,135 @@
 /** 1. ambil inputnya (number) => jumlah bar yg ditampilkan (length of array)
  *  2. buat toggle button start & stop
- *  3. push math random 1-100 ke array of bar value  
- *  4. ganti warna berdasarkan value masing2 bar
- *  5. canvas destroy tiap klik stop
+ *  3. canvas destroy tiap klik stop
+ *  4. push math random 1-100 ke array of bar value
+ *  5. ganti warna berdasarkan value masing2 bar
  */
 
 $(document).ready(()=> {
-  $("#btn-stop").hide()
+    $("#btn-stop").hide()
 
-  $("#btn-start").on("click", (e) => {
-    e.preventDefault()
-    updateChart()
-    $("#btn-stop").show()
-    $("#btn-start").hide()
-  })
+    $("#btn-start").on("click", (e) => {
+        e.preventDefault()
+        updateChart()
+        $("#btn-stop").show()
+        $("#btn-start").hide()
+    })
 
-   const config = {
-    type: 'bar',
-    data: {
-    labels,
-    datasets: [{
-      label: 'Data sets',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [], // = value for each bar
-    }]
-  },
-    options: {}
-  };
+    const config = {
+        type: 'bar',
+        data: {
+        labels,
+        datasets: [{
+        label: 'Data sets',
+        backgroundColor: [],
+        borderColor: [],
+        data: [], // = value for each bar
+        }]
+    },
+        options: {}
+    };
 
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
 })
 
 
-
-
-function stopChart() {
-  // console.log("clear chart")
-  clearTimeout(myTimeout);
-}
-
 function addData(chart, labels) {
-  const newChartData = [];
-  // generate random number from 1 to 100
-  for (const _ of labels) {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
-    newChartData.push(randomNum);
-  }
+    const newChartData = [];
+    // generate random number from 1 to 100
+    for (const _ of labels) {
+        const randomNum = Math.floor(Math.random() * 100) + 1;
+        newChartData.push(randomNum);
+    }
 
-  chart.data.datasets.forEach((dataset) => {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = newChartData;
+    });
+
+    chart.update();
+    }
+
+function changeColor(chart, labels) {
+    const newChartData = [];
+
+    
+    for (const _ of labels) {
+        const randomNum = Math.floor(Math.random() * 100) + 1; // start from 1 to 100;
+        newChartData.push(randomNum);
+    }
+
+    chart.data.datasets.forEach((dataset) => {
+        // dataset.data = newChartData;
+        const bgColor = [];
+
+        console.log(dataset)
+        console.log(dataset.data) // array of bar value
+        console.log(dataset.backgroundColor) //bgcolor
+
+        for (let i = 0; i < dataset.data.length; i++) {
+        console.log(dataset.data[i], "value di dlm array")
+        if (dataset.data[i] >= 0 && dataset.data[i] <= 25) {
+        bgColor.push('rgb(9,128,17)')
+        } else if (dataset.data[i] >= 26 && dataset.data[i] <= 50 ) {
+        bgColor.push('rgb(255,255,15)')
+        } else if (dataset.data[i] >= 51 && dataset.data[i] <= 75) {
+        bgColor.push('rgb(255,0,0)')
+        } else if (dataset.data[i] <= 76 && dataset.data[i] <= 100) {
+        bgColor.push('rgb(0,0,255)')
+        }
+    }
+    // console.log(bgColor)
     dataset.data = newChartData;
-  });
-  
-  chart.update();
-}
+    dataset.backgroundColor = bgColor;
+});
+    chart.update();
+
+  }
 
 function updateChart() {
-  console.log("masuk update")
-  let labels = [];
-  const chartLength = isNaN($("#num-input").val()) ? 0 : Number($("#num-input").val());
-  for (let i = 1; i <= chartLength; i++) {
-    labels.push(i)
-  }
+    console.log("masuk update")
+    let labels = [];
+    const chartLength = isNaN($("#num-input").val()) ? 0 : Number($("#num-input").val());
+    for (let i = 1; i <= chartLength; i++) {
+        labels.push(i)
+     }
 
-  const data = {
-    labels,
-    datasets: [{
-      label: 'Data sets',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [], // = value for each bar
-    }]
-  };
+    const config = {
+        type: 'bar',
+        data: {
+        labels,
+        datasets: [{
+            label: 'Data sets',
+            backgroundColor: [], // 'rgb(255, 99, 132)' tadinya ini
+            borderColor: [],
+            data: [], // = value for each bar
+            }],
+            options: {}
+        }
+    }
 
-
-  const config = {
-    type: 'bar',
-    data: data,
-    options: {}
-  };
-
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
 
 
    // auto generate data in 3 seconds
-  const genRandomEvery3Sec = setInterval(() => {
-    addData(myChart, labels)
-  }, 3000)
+    const genRandomEvery3Sec = setInterval(() => {
+        addData(myChart, labels)
+        changeColor(myChart, labels)
+    }, 3000)
 
-  // if button is clicked, clear timeout => it means random generation will be stopped
-  $("#btn-stop").on('click', () => {
-    $("#btn-start").show()
-    $("#btn-stop").hide()
-    $("#num-input").val('')
+    $("#btn-stop").on('click', () => {
+        $("#btn-start").show()
+        $("#btn-stop").hide()
+        $("#num-input").val('')
 
-    clearInterval(genRandomEvery3Sec);
-    myChart.destroy();
-  })
+        clearInterval(genRandomEvery3Sec);
+        myChart.destroy();
+    })
+
+
 }
